47e1bf6c1866604e0c7ebb36e4b1e2c04ef38128
Manu Sharma <manusharma2462@gmail.com>
2026-02-16 10:39:18 +0530

    changes to deploy

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,22 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 import { authRouter } from "./routes/authRouter";
 import { contestsRouter } from "./routes/contestsRouter";
 import { problemRouter } from "./routes/problemRouter";
+
 app.use("/api/auth", authRouter);
 app.use("/api/contests", contestsRouter);
 app.use("/api/problems", problemRouter);
-app.listen(3000);
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
